87989a16e6be2b0693866ff5b2d0343a37d820aa
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-05-03 20:09:49 -0300

    Solução iterativa

--- src/application/Geen.java
+++ src/application/Geen.java
@@ -3,8 +3,23 @@ package application;
 public class Geen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		fibonacci(10);
 
 	}
-
+	
+	public static int fibonacci(int n) {
+		
+		int ultimo = 1;
+		int penultimo = 0;
+		int i = 3;
+		int seguinte;
+		
+		while(n > i) {
+			seguinte = ultimo + penultimo;
+			penultimo = ultimo;
+			i++;
+		}
+		return n;
+	}
 }
