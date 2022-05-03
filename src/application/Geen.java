038acd8a3193b0c70994fe897359cff40e2a0826
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-05-03 20:20:52 -0300

    Mais correções na solução iterativa - saída do vetor.

--- src/application/Geen.java
+++ src/application/Geen.java
@@ -3,30 +3,33 @@ package application;
 public class Geen {
 
 	public static void main(String[] args) {
-		
-		System.out.println(fibonacci(20));
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(fibonacci(i) + ", ");
+		}
 
 	}
-	
+
 	public static int fibonacci(int n) {
-		
+
 		int ultimo = 1;
 		int penultimo = 0;
 		int i = 3;
 		int seguinte;
-		
-		if(n == 1) {
+
+		if (n == 1) {
 			return 0;
 		}
-		if(n == 2) {
+		if (n == 2) {
 			return 1;
 		}
-		
-		while(n > i) {
+
+		while (n >= i) {
 			seguinte = ultimo + penultimo;
 			penultimo = ultimo;
+			ultimo = seguinte;
 			i++;
 		}
-		return n;
+		return ultimo;
 	}
 }
