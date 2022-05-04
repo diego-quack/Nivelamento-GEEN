f2196035f2d4d57d3d6705abc3b5daa07888b634
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-05-03 22:48:46 -0300

    Modificações e correções no método ordenar.

--- src/application/OrdenarVetor.java
+++ src/application/OrdenarVetor.java
@@ -1,32 +1,43 @@
 package application;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+
 public class OrdenarVetor {
 
 	public static void main(String[] args) {
 		
+		SecureRandom numeros = new SecureRandom();
+		int[] vetor = new int[10];
+		int i;
 		
+		for (i = 0; i < vetor.length; i++) {
+			vetor[i] = 10 + numeros.nextInt(90);
+		}
+		System.out.println(Arrays.toString(vetor));
+		ordenar(0, vetor.length - 1, vetor);
+		System.out.println(Arrays.toString(vetor));
 	}
-	
+
 	public static void ordenar(int inicio, int fim, int[] vetor) {
-		
+
 		int temp;
-		
-		if(inicio < fim) {
+
+		if (inicio < fim) {
 			temp = dividirVetor(inicio, fim, vetor);
 			ordenar(inicio, temp - 1, vetor);
 			ordenar(temp + 1, fim, vetor);
 		}
-		
 	}
-	
+
 	public static int dividirVetor(int inicio, int fim, int[] vetor) {
-		
+
 		int j = fim;
 		int auxiliar;
 		int pivo = 0;
-		
-		for(int i = fim; i >= inicio; i--) {
-			if(vetor[i] > pivo) {
+
+		for (int i = fim; i >= inicio; i--) {
+			if (vetor[i] > pivo) {
 				auxiliar = vetor[j];
 				vetor[j] = vetor[i];
 				vetor[i] = auxiliar;
