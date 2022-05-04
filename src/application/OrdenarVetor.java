766626b4f70a7ecfb8fa79d65f7fdd21601a932f
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-05-03 21:37:15 -0300

    Método para dividir(particionar) o vetor.

--- src/application/OrdenarVetor.java
+++ src/application/OrdenarVetor.java
@@ -6,5 +6,21 @@ public class OrdenarVetor {
 		
 		
 	}
-
+	
+	public static int dividirVetor(int inicio, int fim, int[] vetor) {
+		
+		int j = fim;
+		int auxiliar;
+		int pivo = 0;
+		
+		for(int i = fim; i >= inicio; i--) {
+			if(vetor[i] > pivo) {
+				auxiliar = vetor[j];
+				vetor[j] = vetor[i];
+				vetor[i] = auxiliar;
+				j--;
+			}
+		}
+		return j + 1;
+	}
 }
